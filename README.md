3237698f00a469f9750d94e1666fa788ea2902cf
ohhGHST <salikluis@outlook.com>
2025-04-01 20:01:50 -0500

    Add token loading method

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Discord-Color-App
+
 Discord application used to make user's messages into ANSI formatted colorful code block.
---
+++ bot.py
@@ -0,0 +1,6 @@
+import os
+import discord
+from discord import app_commands
+
+# Load Discord bot token
+TOKEN = os.getenv('DISCORD_TOKEN')
