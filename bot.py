801f99d05b0bb47e72a8c000f345e70d94afd4b7
ohhGHST <salikluis@outlook.com>
2025-04-01 20:02:06 -0500

    Add format options

--- bot.py
+++ bot.py
@@ -4,3 +4,10 @@ from discord import app_commands
 
 # Load Discord bot token
 TOKEN = os.getenv('DISCORD_TOKEN')
+
+# Format options
+FORMAT_OPTIONS = [
+    app_commands.Choice(name="Normal", value=0),
+    app_commands.Choice(name="Bold", value=1),
+    app_commands.Choice(name="Underline", value=4)
+]
