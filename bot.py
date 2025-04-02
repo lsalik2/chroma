d4d9c911690f02b9eb2719e894a3610369ff7f9b
ohhGHST <salikluis@outlook.com>
2025-04-01 20:02:18 -0500

    Add text color options

--- bot.py
+++ bot.py
@@ -11,3 +11,15 @@ FORMAT_OPTIONS = [
     app_commands.Choice(name="Bold", value=1),
     app_commands.Choice(name="Underline", value=4)
 ]
+
+# Text color options
+TEXT_COLORS = [
+    app_commands.Choice(name="Grey", value=30),
+    app_commands.Choice(name="Red", value=31),
+    app_commands.Choice(name="Green", value=32),
+    app_commands.Choice(name="Yellow", value=33),
+    app_commands.Choice(name="Blue", value=34),
+    app_commands.Choice(name="Pink", value=35),
+    app_commands.Choice(name="Cyan", value=36),
+    app_commands.Choice(name="White", value=37)
+]
