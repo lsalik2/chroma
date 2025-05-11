13101edc4fb52c9c6ae765818b0fd7db7a8cae71
ohhGHST <salikluis@outlook.com>
2025-05-10 23:47:51 -0500

    MatchStatus class

--- models/tournament.py
+++ models/tournament.py
@@ -9,3 +9,9 @@ class TeamStatus(Enum):
     PENDING = "pending"   # Awaiting admin approval
     APPROVED = "approved" # Approved and part of tournament
     DENIED = "denied"     # Denied by admin
+
+class MatchStatus(Enum):
+    PENDING = "pending"     # Match not started
+    IN_PROGRESS = "playing" # Match in progress
+    COMPLETED = "completed" # Match completed
+    FORFEIT = "forfeit"     # Match forfeited
