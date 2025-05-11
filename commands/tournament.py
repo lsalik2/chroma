3d70b217f7a09a770b2ce07a7ae00f693096273d
ohhGHST <salikluis@outlook.com>
2025-05-11 00:31:42 -0500

    CHANNEL_EMOJI_MAP and PASSWORDS constants

---
+++ commands/tournament.py
@@ -0,0 +1,16 @@
+# Constants
+CHANNEL_EMOJI_MAP = {
+    "announcements": "📢",
+    "rules": "📃",
+    "bracket": "🏆",
+    "sign_up": "✒️",
+    "lobby": "🗣️",
+    "questions": "❓",
+}
+
+PASSWORDS = [
+    "Vex", "Drift", "Flux", "Nova", "Blitz", "Zyn", "Wisp", "Rune",
+    "Axe", "Glint", "Crux", "Jynx", "Nyx", "Fang", "Hex", "Void",
+    "Echo", "Pyre", "Grim", "Keen", "Raze", "Obel", "Shiv", "Zeph",
+    "Talon", "Nox"
+]
