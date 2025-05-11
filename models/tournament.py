68156c380191027dc4ef005c0515c4b6c8b31ac8
ohhGHST <salikluis@outlook.com>
2025-05-10 23:47:34 -0500

    TeamStatus class

--- models/tournament.py
+++ models/tournament.py
@@ -4,3 +4,8 @@ class TournamentFormat(Enum):
     CHOOSE = "choose"   # Players choose their team
     BALANCE = "balance" # Bot balances teams
     RANDOM = "random"   # Bot creates random teams
+
+class TeamStatus(Enum):
+    PENDING = "pending"   # Awaiting admin approval
+    APPROVED = "approved" # Approved and part of tournament
+    DENIED = "denied"     # Denied by admin
