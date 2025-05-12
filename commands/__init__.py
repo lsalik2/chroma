c0e06685c7d358494537c88a1980e84e8597032d
ohhGHST <salikluis@outlook.com>
2025-05-12 15:12:31 -0500

    Update commands __init__.py

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,10 +1,12 @@
 from .chroma import register_chroma
 from .translate import register_translate
 from .randomize import register_randomize
+from .tournament import register_tournament
 from .admin import register_admin
 
 def setup_commands(tree):
     register_chroma(tree)
     register_translate(tree)
     register_randomize(tree)
+    register_tournament(tree)
     register_admin(tree)
